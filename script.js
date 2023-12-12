c0bdf09ce6569e43265ab80b9694e13f9f6d8302
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-12 13:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 //your JS code here. If required.
 function functionName(); {
-	alert("functionName");
+	alert(arguments.callee.name);
 	
 }
 functionName();
-function AccioJob(); {
-	alert("AccioJob");
-}
-AccioJob();
